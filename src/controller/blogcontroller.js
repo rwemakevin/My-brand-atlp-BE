120dbb713f8c07dba3883818216a51b81bb6df82
KevinRwema <kevinrwema@gmail.com>
2024-03-01 13:58:52 +0200

    finalize viewBlogById endpoint

--- src/controller/blogcontroller.js
+++ src/controller/blogcontroller.js
@@ -28,12 +28,36 @@ export default class blogController {
       const allBlogs = await blogSchema.find();
       return res.status(200).json({
         status: "Success",
+        totalBlogs: allBlogs.length,
         data: allBlogs,
       });
     } catch (error) {
       return res.status(500).json({
         status: "fail",
         message: "something went wrong " + error,
+      });
+    }
+  }
+
+  static async viewBlogById(req, res) {
+    try {
+      const id = req.params.id;
+      const blog = await blogSchema.findById(id);
+      if (!blog) {
+        return res.status(500).json({
+          status: "fail",
+          message: "Blog not found",
+        });
+      }
+
+      return res.status(200).json({
+        status: "success",
+        data: blog,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        status: "Fail",
+        message: "Something went wrong: " + error,
       });
     }
   }
--- src/routes/blogroute.js
+++ src/routes/blogroute.js
@@ -4,5 +4,6 @@ import blogController from "../controller/blogcontroller.js";
 const blog = Router();
 blog.post("/blog", blogController.addBlog);
 blog.get("/blog", blogController.viewBlogs);
+blog.get("/blog/:id", blogController.viewBlogById);
 
 export default blog;
