e1180f75f7563be8d0134246e3c560bb4ca0b27b
KevinRwema <kevinrwema@gmail.com>
2024-03-07 00:29:35 +0200

    fx: message: add roles to all endpoints

--- src/routes/messageroute.js
+++ src/routes/messageroute.js
@@ -1,12 +1,35 @@
 import { Router } from "express";
 
 import messageController from "../controller/messagecontroller.js";
+import authMiddleWare from "../middleware/authMiddleware.js";
 
 const message = Router();
 
+// Send message via contact form | Public endpoint
 message.post("/messages", messageController.sendMessage);
-message.get("/messages", messageController.viewMessages);
-message.get("/messages/:id", messageController.viewMessageById);
-message.delete("/messages/:id", messageController.deleteMessageById);
+
+// View all messages | private endpoint
+message.get(
+  "/messages",
+  authMiddleWare.isAuthenticated,
+  authMiddleWare.level3Role,
+  messageController.viewMessages
+);
+
+//view one message | private endpoint
+message.get(
+  "/messages/:id",
+  authMiddleWare.isAuthenticated,
+  authMiddleWare.level3Role,
+  messageController.viewMessageById
+);
+
+//Delete one message | private endpoint
+message.delete(
+  "/messages/:id",
+  authMiddleWare.isAuthenticated,
+  authMiddleWare.level4Role,
+  messageController.deleteMessageById
+);
 
 export default message;
